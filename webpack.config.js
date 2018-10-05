b001c7041b71536106f26ea3352e05ab2680ac7e
Donato Azevedo <donatoaz@gmail.com>
2018-10-05 19:52:08 -0300

    Fixes webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,18 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: ['babel-loader'],
       include: path.join(__dirname, 'src')
     },
     {
